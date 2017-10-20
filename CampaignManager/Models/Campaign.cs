d6a65ab77ebe2689fd0cb0d3973fb3a7b5eff54f
jomanrones <rjon141@lsu.edu>
2017-10-20 07:13:18 -0500

    Check in 10/20
    Added some UI to the home screeen and some new models

---
+++ CampaignManager/Models/Campaign.cs
@@ -0,0 +1,33 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampaignManager.Models
+{
+    public class Campaign : ObservableObject
+    {
+        private Guid id;
+        public Guid Id
+        {
+            get { return id; }
+            set { Set(() => Id, ref id, value); }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { Set(() => Name, ref name, value); }
+        }
+
+        private byte[] image;
+        public byte[] Image
+        {
+            get { return image; }
+            set { Set(() => Image, ref image, value); }
+        }
+    }
+}
--- CampaignManager/ViewModels/HomePageViewModel.cs
+++ CampaignManager/ViewModels/HomePageViewModel.cs
@@ -1,13 +1,44 @@
 ﻿using System;
 
 using GalaSoft.MvvmLight;
+using System.Collections.ObjectModel;
+using CampaignManager.Models;
 
 namespace CampaignManager.ViewModels
 {
     public class HomePageViewModel : ViewModelBase
     {
+        private ObservableCollection<Campaign> campaigns = new ObservableCollection<Campaign>();
+        public ObservableCollection<Campaign> Campaigns
+        {
+            get { return campaigns; }
+            set { Set(() => Campaigns, ref campaigns, value); }
+        }
+
+        private ObservableCollection<Character> characters = new ObservableCollection<Character>();
+        public ObservableCollection<Character> Characters
+        {
+            get { return characters; }
+            set { Set(() => Characters, ref characters, value); }
+        }
+
+        private ObservableCollection<Monster> monsters = new ObservableCollection<Monster>();
+        public ObservableCollection<Monster> Monsters
+        {
+            get { return monsters; }
+            set { Set(() => monsters, ref monsters, value); }
+        }
+
+        private ObservableCollection<Item> items = new ObservableCollection<Item>();
+        public ObservableCollection<Item> Items
+        {
+            get { return items; }
+            set { Set(() => Items, ref items, value); }
+        }
+
         public HomePageViewModel()
         {
+            
         }
     }
 }
