d96387e53da0d596568b42c8ef90f4c1eeee4310
jomanrones <32491852+jomanrones@users.noreply.github.com>
2017-11-14 15:41:37 -0600

    a
    a

--- CampaignManager/Models/Alignment.cs
+++ CampaignManager/Models/Alignment.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CampaignManager.Models
+﻿namespace CampaignManager.Models
 {
     public enum Alignment
     {
