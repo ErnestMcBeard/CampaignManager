ada8b5a1ec8ee79b51a14e52e66358c8208f769f
FedoraLord <jerkrouse@gmail.com>
2017-11-21 20:33:36 -0600

    Campaign Page
    Did a little set up code for campaign page / viewmodel. Tried to copy what was done for homepage and in the VM locater class. Couldn't find where the OnNavigation method was.

--- CampaignManager/ViewModels/ViewModelLocator.cs
+++ CampaignManager/ViewModels/ViewModelLocator.cs
@@ -14,10 +14,12 @@ namespace CampaignManager.ViewModels
             SimpleIoc.Default.Register(() => new NavigationServiceEx());
             Register<HomeViewModel, HomePage>();
             Register<AddCharacterViewModel, AddCharacterPage>();
+            Register<CampaignViewModel, CampaignPage>();
         }
 
         public HomeViewModel HomePageViewModel => ServiceLocator.Current.GetInstance<HomeViewModel>();
         public AddCharacterViewModel AddCharacterViewModel => ServiceLocator.Current.GetInstance<AddCharacterViewModel>();
+        public CampaignViewModel CampaignPageViewModel => ServiceLocator.Current.GetInstance<CampaignViewModel>();
         public NavigationServiceEx NavigationService => ServiceLocator.Current.GetInstance<NavigationServiceEx>();
 
         public void Register<VM, V>() where VM : class
