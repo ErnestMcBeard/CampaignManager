3dfa929271a8e0c2528708f8f3530e83008bf4fa
FedoraLord <jerkrouse@gmail.com>
2017-11-24 19:54:24 -0600

    Made encounter listview template

---
+++ CampaignManager/Converters/CompletedTextConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace CampaignManager.Converters
+{
+    public class CompletedTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if ((bool)value)
+                return "Complete";
+            return String.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
